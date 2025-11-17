05a0a7ace91b27f89571fe9f187f9ecc1635bcf5
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-11-17 10:19:38 +0300

    update many.py

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -37,27 +37,57 @@ class Author:
         return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        magazines = self.magazines()
+        if not magazines:
+            return None
+        return list(set(magazine.category for magazine in magazines))
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        pass
+        articles = self.articles()
+        if not articles:
+            return None
+        return [article.title for article in articles]
 
     def contributing_authors(self):
-        pass
+        articles = self.articles()
+        if not articles:
+            return None
+        authors = [author for author in self.contributors() if len([article for article in author.articles() if article.magazine == self]) > 2]
+        if not authors:
+            return None
+        return authors
